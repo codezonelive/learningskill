bdfab76c363461cdf67d3c6e6da02d0361d18b9f
Kanchit <mr.kk0000@gmail.com>
2020-06-09 19:30:11 +0700

    test

--- demo_redux_hooks/src/component/Testredux.js
+++ demo_redux_hooks/src/component/Testredux.js
@@ -1,12 +1,32 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React ,{useState} from 'react'
+import { addcouter, delcounter } from '../action/counteraction'
+import { useSelector, useDispatch } from 'react-redux'
  
 
 const Testredux = () =>{
-    const { count } = useSelector(state => state.counter)
+    const { count } = useSelector(state => state.counter )
+    const dispatch = useDispatch()
+    const [txtss, setText] = useState({
+        text: 'sss'
+    })
+
+
+    // const handlText = () =>{
+       
+    // }
+
+    const handleSend = () =>{
+        setText({
+            text: ''
+        })
+        // dispatch(addcouter())
+    }
+
     return (
         <div className="testredux">
             <h1> count : { count }</h1>
+            <input type="text"  value={txtss.text }placeholder="test ....."/>
+            <button onClick={ handleSend }>test send back</button>
         </div>
     )
 }
